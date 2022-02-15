cf5c154815312dcc8f98f63bef2f816ccb4f918d
adornala16382 <R.E.N.dornala@gmail.com>
2022-02-14 23:29:08 -0500

    added comments

--- WordleSolver.py
+++ WordleSolver.py
@@ -31,6 +31,7 @@ class WordleSolver:
     correctSpots = []
     wrongSpots = []
 
+# initializes game
     def __init__(self, hardCodedAnswer, runStatistics):
         self.window = tk.Tk()
         self.window.title("Wordle Solver")
@@ -44,7 +45,8 @@ class WordleSolver:
             self.statistics()
         else:
             self.restart()
-    
+
+# restarts game   
     def restart(self):
         self.allowedGuesses = all_words.copy()
         self.guess = ""
@@ -77,15 +79,16 @@ class WordleSolver:
         if(self.runStatistics == False):
             print("answer: "+self.answer)
 
+# Creates grid
     def createGrid(self):
         self.canvas.create_rectangle(board_width / 2 - 187.5, board_height / 12 + (75), board_width / 2 - 187.5 + (5*75), board_height / 12 + (7 *75),  fill="white")
         for i in range(7):
             self.canvas.create_line(board_width / 2 - 187.5, board_height / 12 + (75*(i+1)), board_width / 2 + 187.5, board_height / 12 + (75*(i+1)), fill='black', width=2)
         for i in range(6):
             self.canvas.create_line(board_width / 2 - 187.5 + (i*75), board_height / 12 + 75, board_width / 2 - 187.5 + (i*75), board_height / 12 + (75*7), fill='black', width=2)
-        
+
+# Determine if switch is on or off        
     def switch(self):   
-        # Determine is on or off
         if(self.guess_count==0):
             if self.is_on:
                 self.on_button.config(image = self.off)
@@ -96,6 +99,7 @@ class WordleSolver:
                 self.canvas.create_text(board_width/2, board_height / 12 + 60, font="cmr 10 bold", fill='white', text="Click on letters to change its color")
                 self.answer = ""
 
+# types input text on screen
     def typeInputWord(self):
         self.guess = self.e1.get()
         if(len(self.guess)==5):
@@ -104,8 +108,8 @@ class WordleSolver:
             self.typeWord()
         self.e1.delete(0,tk.END)
 
+# logic to guess the next word
     def guessWord(self):
-          
         self.deleteList = []
         if(len(self.allowedGuesses) >= 1):
             if(self.guess_count <= 5 and self.victory==False):
@@ -122,6 +126,7 @@ class WordleSolver:
                 
                 self.determineVictory()
 
+# determines whether or not the computer won and displays victory/defeat screen
     def determineVictory(self):
         if(self.guess == self.answer):
             self.canvas.create_text(board_width / 2,  board_height / 12 + 35, font="cmr 20 bold", fill='green', text='VICTORY')
@@ -133,7 +138,7 @@ class WordleSolver:
             self.victory = False
             self.guess_count = 7
 
-    
+# logic for assistMode to narrow down the possible word list
     def assistMode(self):
         for i, char in enumerate(self.guess):
             if(self.currentWord[i][1] %3 == 2):
@@ -154,7 +159,8 @@ class WordleSolver:
         num = self.predict()
         self.guess = self.allowedGuesses[num]
         self.deleteList.append(num)
-    
+
+# logic for computerMode to narrow down the possible word list   
     def computerMode(self):
         num = self.predict()
         self.guess = self.allowedGuesses[num]
@@ -177,6 +183,7 @@ class WordleSolver:
                         self.deleteList.append(j)
         self.deleteWords()
 
+# changes the color of a tile when clicked on
     def change_color(self, *args):
         x = (args[0].x - 415) // 75
         y = ((args[0].y - 138) // 75) + 1
@@ -188,7 +195,7 @@ class WordleSolver:
                 self.canvas.itemconfig(self.currentWord[x][0], fill='yellow')
             else:
                 self.canvas.itemconfig(self.currentWord[x][0], fill='green')
-
+# types out word on screen
     def typeWord(self):
         self.currentWord = []
         for i, char in enumerate(self.guess):
@@ -201,13 +208,14 @@ class WordleSolver:
             self.canvas.create_text(board_width / 2 - 150 + (i*75), board_height / 12 + 35 + (self.guess_count*75), font="cmr 30 bold", fill='black', text=char.upper(), tags = f"curWord{i}")
             self.canvas.tag_bind(f"curWord{i}","<Button-1>",self.change_color) 
 
+# deletes words that can't be the answer from the list of possible guesses
     def deleteWords(self):
         self.deleteList = list(set(self.deleteList))
         self.deleteList.sort(reverse=True)
         for index in self.deleteList:
             del self.allowedGuesses[index]
 
-    #predicts word
+# predicts the answer
     def predict(self):
         if(len(self.allowedGuesses)>= 2):
             max_len = -1
@@ -228,6 +236,7 @@ class WordleSolver:
             num = random.randint(0,len(self.allowedGuesses)-1)
             return num
 
+# runs statistics
     def statistics(self):
         scoreSum = 0
         for i in range(len(all_words)):      
@@ -244,7 +253,9 @@ class WordleSolver:
     def run(self):
         self.window.mainloop()
 
-
+# Runs program
+# Set hardCodedAnswer to a 5 letter word to set the word the computer has to guess
+# Set runStatistics to True to see the average number of guesses the computer takes to get the answer
 if __name__ == "__main__":
-    game_instance = WordleSolver(hardCodedAnswer = None, runStatistics = True)
+    game_instance = WordleSolver(hardCodedAnswer = None, runStatistics = False)
     game_instance.run()
